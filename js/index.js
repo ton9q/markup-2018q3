a5bd7cd470888c6cdfa9a898511a4c69ba489401
ton9q <kuchma_anton@mail.ru>
2018-11-04 22:46:04 +0300

    feat(slider): add slides and logic for slider

--- js/index.js
+++ js/index.js
@@ -8,3 +8,39 @@ menuTop.addEventListener('click', () => {
     x.className = 'top-nav'
   }
 });
+
+
+/* SLIDER */
+const commentsContent = [
+  {
+    comment: '“Suspendisse tempor turpis odio, sit amet cursus leo consequat non. Maecenas lacinia faucibus enimqui interdum dolor pulvinar vitae.”',
+    person: 'Johny Doe',
+  },
+  {
+    comment: '“Suspendisse tempor turpis odio, sit amet cursus leo consequat non.”',
+    person: 'John Doe',
+  },
+  {
+    comment: '“Suspendisse tempor turpis odio.”',
+    person: 'Johnathan Doe',
+  },
+];
+
+const dots = [...document.getElementById('dots').children];
+dots.forEach((item, id) => {
+    item.addEventListener('click', () => {
+        const comment = document.getElementById('comment');
+        const person = document.getElementById('person_name');
+
+        comment.innerHTML = commentsContent[id].comment;
+        person.innerHTML = commentsContent[id].person;
+
+        dots.forEach((dot, index) => {
+            if (index === id) {
+                dot.style.backgroundColor = 'white';
+            } else {
+                dot.style.backgroundColor = 'transparent';
+            }
+        });
+    });
+});
